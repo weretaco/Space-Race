1091b82ac25dde929216b80a45c9877f5ce4f933
dportnoy <dmp1488@gmail.com>
2012-05-08 23:04:52 -0400

    Added a src folder and starting activity and changed the manifest to specify the starting activity.

---
+++ src/com/medievaltech/SpaceRace.java
@@ -0,0 +1,14 @@
+package com.medievaltech;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpaceRace extends Activity {
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
